c001dd9a471356b5a3c4efa3b22c05ab0b2d5248
Sarafaraj Shaikh <sarafarajshaikh@gmail.com>
2024-11-24 15:57:19 +0530

    change241120241557

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -25,7 +25,7 @@ const FileUpload = () => {
 
       const response = await axios.post(
         // "http://localhost:5000/upload",
-        "https://xrdbackend.onrender.com/upload",
+        "https://xrdbackend.onrender.com",
         formData,
         {
           headers: {
